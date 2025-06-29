507e4e3fac1a87e14ae17f71befaa84c79f2ccc7
Ramavtar <126427788+Ramavatar98@users.noreply.github.com>
2025-06-29 07:31:12 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView textView = new TextView(this);
+        textView.setText("Hello from GitHub-built APK!");
+        setContentView(textView);
+    }
+}
